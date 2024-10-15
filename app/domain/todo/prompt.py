dfea92213e3912d9763b5a00b809bb8abfe1fd63
노정완 <jeongwan.noh@gmail.com>
2024-10-15 21:24:34 +0900

    HOTFIX: 프롬프트 수정

--- app/domain/todo/prompt.py
+++ app/domain/todo/prompt.py
@@ -2,22 +2,23 @@ TODO_PROMPT = """
 You are a powerful language model tasked with analyzing texts.
 Your job is to generate to-do items from given texts.
 
-- ToDo Generation
+- **ToDo Generation**
 From the following text, extract up to 5 to-do items, considering their importance.
 Ensure that each task includes all necessary context from the original text, such as names, specific details, 
 or any other information that makes the task clear and unambiguous.
-For each task, include the portion of the original text from which it was extracted.
+
+- **Guidelines:**
+  - Each task should be concise and not exceed 30 characters in length.
+  - **Do not generate multiple tasks from the same portion of text. 
+  Each 'origin' must be associated with only one 'task', and vice versa.**
+  - **Avoid breaking tasks into overly small pieces; combine related actions into a single, meaningful task.**
+  - **Focus on the core aspects of the tasks, emphasizing important details over minor ones.**
+
+For each task, include the specific portion of the original text from which it was extracted.
 Return them as a list of tasks in JSON format that matches the provided schema.
 If there are no to-do items to extract, return an empty list.
+
 MUST:
  return response in KOREAN
- 
-Example:
-Input Text:
-"move 과연 답 올지 기다려봐야될듯"
-
-Extracted Task:
-  "task": "move에게 답변이 오는지 기다리기",
-  "origin": "move 과연 답 올지 기다려봐야될듯"
 
 """
